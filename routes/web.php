c50b0b56b6230b3323e55aff80f67cbc945ce433
Mustafidatun <mustafidatunnashihah@gmail.com>
2019-03-15 21:45:17 +0700

    fix redirect

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('auth/login');
+    if(Auth::check()){
+        return redirect('/home');
+    }else{
+        return view('auth/login');
+    }
 });
 
 
